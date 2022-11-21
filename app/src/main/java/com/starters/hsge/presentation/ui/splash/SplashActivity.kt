a4aece6794eff314c4b2b1c42682c6ebd908370a
seoyoon513 <syoonlee513@gmail.com>
2022-11-21 14:47:43 +0900

    [CHORE] #3 test issue

--- app/src/main/java/com/starters/hsge/presentation/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/starters/hsge/presentation/ui/splash/SplashActivity.kt
@@ -3,6 +3,7 @@ package com.starters.hsge.presentation.ui.splash
 class SplashActivity {
 
     //fragment -> viewmodel -> (uscase) -> repo -> data들.... mvvm => 클린아키텍쳐 /이미 정해진 교과서적인 틀
+    // 위치정보
 
 
 }
